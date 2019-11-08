853f0d6df41e527bda3efd1f3e6bba73aa379775
Brian Rojas <marchinram@gmail.com>
2019-11-07 18:13:35 -0600

    Android current location serialization and other methods

--- android/src/main/java/com/foursquare/pilgrim/react/RNPilgrimSdkModule.java
+++ android/src/main/java/com/foursquare/pilgrim/react/RNPilgrimSdkModule.java
@@ -1,73 +1,74 @@
 
 package com.foursquare.pilgrim.react;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.Promise;
 import com.foursquare.pilgrim.CurrentLocation;
 import com.foursquare.pilgrim.PilgrimNotificationTester;
 import com.foursquare.pilgrim.PilgrimSdk;
 import com.foursquare.pilgrim.Result;
 
+import org.json.JSONException;
+
 public class RNPilgrimSdkModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNPilgrimSdkModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNPilgrimSdkModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNPilgrimSdk";
-  }
+    @Override
+    public String getName() {
+        return "RNPilgrimSdk";
+    }
 
-  @ReactMethod
-  public void isSupportedDevice(Promise promise) {
-    promise.resolve(true);
-  }
+    @ReactMethod
+    public void isSupportedDevice(Promise promise) {
+        promise.resolve(true);
+    }
 
-  @ReactMethod
-  public void canEnable(Promise promise) {
-    promise.resolve(true);
-  }
+    @ReactMethod
+    public void canEnable(Promise promise) {
+        promise.resolve(true);
+    }
 
-  @ReactMethod
-  public void getInstallId(Promise promise) {
-    promise.resolve(PilgrimSdk.getInstallId());
-  }
+    @ReactMethod
+    public void getInstallId(Promise promise) {
+        promise.resolve(PilgrimSdk.getInstallId());
+    }
 
-  @ReactMethod
-  public void start(Promise promise) {
-    PilgrimSdk.start(reactContext);
-  }
+    @ReactMethod
+    public void start(Promise promise) {
+        PilgrimSdk.start(reactContext);
+    }
 
-  @ReactMethod
-  public void stop(Promise promise) {
-    PilgrimSdk.stop(reactContext);
-  }
+    @ReactMethod
+    public void stop(Promise promise) {
+        PilgrimSdk.stop(reactContext);
+    }
 
-  @ReactMethod
-  public void getCurrentLocation(final Promise promise) {
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        Result<CurrentLocation, Exception> currentLocationResult = PilgrimSdk.get().getCurrentLocation();
-        if (currentLocationResult.isOk()) {
-          CurrentLocation currentLocation = currentLocationResult.getResult();
-          promise.resolve(null);
-        } else {
-          promise.reject("E_GET_CURRENT_LOCATION", currentLocationResult.getErr());
-        }
-      }
-    }).start();
-  }
+    @ReactMethod
+    public void getCurrentLocation(final Promise promise) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Result<CurrentLocation, Exception> currentLocationResult = PilgrimSdk.get().getCurrentLocation();
+                if (currentLocationResult.isOk()) {
+                    CurrentLocation currentLocation = currentLocationResult.getResult();
+                    promise.resolve(Utils.currentLocationJson(currentLocation));
+                } else {
+                    promise.reject("E_GET_CURRENT_LOCATION", currentLocationResult.getErr());
+                }
+            }
+        }).start();
+    }
 
-  @ReactMethod
-  public void fireTestVisit(double latitude, double longitude) {
-    new PilgrimNotificationTester().sendTestVisitArrivalAtLocation(reactContext, latitude, longitude, false);
-  }
+    @ReactMethod
+    public void fireTestVisit(double latitude, double longitude) {
+        PilgrimNotificationTester.sendTestVisitArrivalAtLocation(reactContext, latitude, longitude, false);
+    }
 }
---
+++ android/src/main/java/com/foursquare/pilgrim/react/Utils.java
@@ -0,0 +1,239 @@
+package com.foursquare.pilgrim.react;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.foursquare.api.FoursquareLocation;
+import com.foursquare.api.types.Category;
+import com.foursquare.api.types.Photo;
+import com.foursquare.api.types.Venue;
+import com.foursquare.api.types.geofence.GeofenceEvent;
+import com.foursquare.pilgrim.CurrentLocation;
+import com.foursquare.pilgrim.Visit;
+
+import java.util.List;
+
+final class Utils {
+
+    private Utils() {
+
+    }
+
+    static ReadableMap currentLocationJson(CurrentLocation currentLocation) {
+        WritableMap json = Arguments.createMap();
+        json.putMap("currentPlace", visitJson(currentLocation.getCurrentPlace()));
+
+        WritableArray geofenceEvents = Arguments.createArray();
+        for (GeofenceEvent event : currentLocation.getMatchedGeofences()) {
+            geofenceEvents.pushMap(geofenceEventJson(event));
+        }
+        json.putArray("matchedGeofences", geofenceEvents);
+
+        return json;
+    }
+
+    private static ReadableMap visitJson(Visit visit) {
+        WritableMap json = Arguments.createMap();
+        json.putMap("location", foursquareLocationJson(visit.getLocation()));
+
+        int locationType;
+        switch (visit.getType()) {
+            case HOME:
+                locationType = 1;
+                break;
+            case WORK:
+                locationType = 2;
+                break;
+            case VENUE:
+                locationType = 3;
+                break;
+            default:
+                locationType = 0;
+                break;
+        }
+        json.putInt("locationType", locationType);
+
+        int confidence;
+        switch (visit.getConfidence()) {
+            case LOW:
+                confidence = 1;
+                break;
+            case MEDIUM:
+                confidence = 2;
+                break;
+            case HIGH:
+                confidence = 3;
+                break;
+            default:
+                confidence = 0;
+                break;
+        }
+        json.putInt("confidence", confidence);
+
+        json.putDouble("arrivalTime", visit.getArrival() / 1000);
+
+        if (visit.getVenue() != null) {
+            json.putMap("venue", venueJson(visit.getVenue()));
+        }
+
+        WritableArray otherPossibleVenuesArray = Arguments.createArray();
+        for (Venue venue : visit.getOtherPossibleVenues()) {
+            otherPossibleVenuesArray.pushMap(venueJson(venue));
+        }
+        json.putArray("otherPossibleVenues", otherPossibleVenuesArray);
+
+        return json;
+    }
+
+    private static ReadableMap geofenceEventJson(GeofenceEvent geofenceEvent) {
+        WritableMap json = Arguments.createMap();
+        json.putString("id", geofenceEvent.getId());
+
+        if (geofenceEvent.getVenue() != null) {
+            Venue venue = geofenceEvent.getVenue();
+            json.putString("venueId", venue.getId());
+            json.putMap("venue", venueJson(venue));
+        }
+
+        if (geofenceEvent.getPartnerVenueId() != null) {
+            json.putString("partnerVenueId", geofenceEvent.getPartnerVenueId());
+        }
+
+        json.putMap("location", locationJson(geofenceEvent.getLat(), geofenceEvent.getLng()));
+        json.putDouble("timestamp", geofenceEvent.getTimestamp() / 1000);
+        return json;
+    }
+
+    private static ReadableMap chainJson(Venue.VenueChain chain) {
+        WritableMap json = Arguments.createMap();
+        json.putString("id", chain.getId());
+        json.putString("name", chain.getName());
+        return json;
+    }
+
+    private static ReadableArray chainsArrayJson(List<Venue.VenueChain> chains) {
+        WritableArray json = Arguments.createArray();
+        if (chains != null) {
+            for (Venue.VenueChain chain : chains) {
+                json.pushMap(chainJson(chain));
+            }
+        }
+        return json;
+    }
+
+    private static ReadableArray categoryArrayJson(List<Category> categories) {
+        WritableArray json = Arguments.createArray();
+        for (Category category : categories) {
+            json.pushMap(categoryJson(category));
+        }
+        return json;
+    }
+
+    private static ReadableMap categoryJson(Category category) {
+        WritableMap json = Arguments.createMap();
+        json.putString("id", category.getId());
+        json.putString("name", category.getName());
+
+        if (category.getPluralName() != null) {
+            json.putString("pluralName", category.getPluralName());
+        }
+
+        if (category.getShortName() != null) {
+            json.putString("shortName", category.getShortName());
+        }
+
+        if (category.getImage() != null) {
+            json.putMap("icon", categoryIconJson(category.getImage()));
+        }
+
+        json.putBoolean("isPrimary", category.isPrimary());
+
+        return json;
+    }
+
+    private static ReadableMap categoryIconJson(Photo photo) {
+        WritableMap json = Arguments.createMap();
+        json.putString("prefix", photo.getPrefix());
+        json.putString("suffix", photo.getSuffix());
+        return json;
+    }
+
+    private static ReadableArray hierarchyJson(List<Venue.VenueParent> hierarchy) {
+        WritableArray json = Arguments.createArray();
+        if (hierarchy != null) {
+            for (Venue.VenueParent parent : hierarchy) {
+                WritableMap parentJson = Arguments.createMap();
+                parentJson.putString("id", parent.getId());
+                parentJson.putString("name", parent.getName());
+                parentJson.putArray("categories", categoryArrayJson(parent.getCategories()));
+                json.pushMap(parentJson);
+            }
+        }
+        return json;
+    }
+
+    private static ReadableMap venueJson(Venue venue) {
+        WritableMap json = Arguments.createMap();
+        json.putString("id", venue.getId());
+        json.putString("name", venue.getName());
+
+        if (venue.getLocation() != null) {
+            json.putMap("locationInformation", venueLocationJson(venue.getLocation()));
+        }
+
+        if (venue.getPartnerVenueId() != null) {
+            json.putString("partnerVenueId", venue.getPartnerVenueId());
+        }
+
+        json.putDouble("probability", venue.getProbability());
+
+
+        json.putArray("chains", chainsArrayJson(venue.getVenueChains()));
+
+        json.putArray("categories", categoryArrayJson(venue.getCategories()));
+
+        json.putArray("hierarchy", hierarchyJson(venue.getHierarchy()));
+
+        return json;
+    }
+
+    private static ReadableMap venueLocationJson(Venue.Location venueLocation) {
+        WritableMap json = Arguments.createMap();
+
+        if (venueLocation.getAddress() != null) {
+            json.putString("address", venueLocation.getAddress());
+        }
+        if (venueLocation.getCrossStreet() != null) {
+            json.putString("crossStreet", venueLocation.getCrossStreet());
+        }
+        if (venueLocation.getCity() != null) {
+            json.putString("city", venueLocation.getCity());
+        }
+        if (venueLocation.getState() != null) {
+            json.putString("state", venueLocation.getState());
+        }
+        if (venueLocation.getPostalCode() != null) {
+            json.putString("postalCode", venueLocation.getPostalCode());
+        }
+        if (venueLocation.getCountry() != null) {
+            json.putString("country", venueLocation.getCountry());
+        }
+        json.putMap("location", locationJson(venueLocation.getLat(), venueLocation.getLng()));
+
+        return json;
+    }
+
+    private static ReadableMap foursquareLocationJson(FoursquareLocation foursquareLocation) {
+        return locationJson(foursquareLocation.getLat(), foursquareLocation.getLng());
+    }
+
+    private static ReadableMap locationJson(double lat, double lng) {
+        WritableMap json = Arguments.createMap();
+        json.putDouble("latitude", lat);
+        json.putDouble("longitude", lng);
+        return json;
+    }
+
+}
